91530d9d7e0215dd5cb66233527a64c7efc94141
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-01-28 13:44:30 +0000

    break

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -4,20 +4,16 @@ import macbook from '../images/MacBook.png';
 function Hero() {
   return (
     <>
-    <div>
-        <div className='text-center text-slate-950 text-4xl'>
+    <div className='max-w-7xl mx-auto'>
+        <div className='flex text-slate-950 text-6xl'>
         Atlas Premier <br/> Knowledge Blogs:
+        <p className=' mt-3 text-lg'>"Immerse yourself in the latest knowledge and research from our team of software engineers and design professionals.</p>
+
         </div>
-        <p className='text-center mt-3'>"Immerse yourself in the latest knowledge and research from our team of software engineers and design professionals.</p>
     </div>
 
-<div>
-  <div className='w-11/12 h-96 bg-slate-800 rounded mx-auto mt-3'>
-    <div className='w-10/12 h-80 bg-black rounded mx-auto mt-3'>
-    <img className=' h-full object-contain' src={macbook.src} alt="Macbook" />
-    </div>
-</div>
-</div>
+
+
 
 </>
   )
