528ac462c765e92d1fad27dc3fd1516ef67586be
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-01-31 16:13:30 +0000

    hero text

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -1,22 +1,28 @@
-import React from 'react'
-import macbook from '../images/MacBook.png';
+import React from "react";
+import macbook from "../images/MacBook.png";
 
 function Hero() {
   return (
     <>
-    <div className='max-w-7xl mx-auto'>
-        <div className='flex text-slate-950 text-6xl'>
-        Atlas Premier <br/> Knowledge Blogs:
-        <p className=' mt-3 text-lg'>"Immerse yourself in the latest knowledge and research from our team of software engineers and design professionals.</p>
-
+      <div className="max-w-7xl mx-auto">
+        <div className="flex flex-col items-center text-center sm:flex-row sm:text-left text-slate-950 text-4xl ">
+          <div className="sm:w-1/2 font-bold">
+            Atlas Premier <br /> Knowledge Blogs:
+          </div>
+          <div className="sm:w-1/2">
+            <p className="mt-3 text-base">
+              "Immerse yourself in the latest knowledge and research from our
+              team of software engineers and design professionals.
+            </p>
+          </div>
         </div>
-    </div>
 
-
-
-
-</>
-  )
+        <div>
+          
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
