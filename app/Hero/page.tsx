9b5be508b1d3a4672997f1bee99bbff581152e39
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-01 14:43:43 +0000

    cardd

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -16,26 +16,11 @@ function Hero() {
             </p>
           </div>
         </div>
-        {/* <div className="mt-8 m-3">
-          <div className="bg-blue-950 rounded-2xl p-2 ">
-           
-            <div className="bg-black rounded-lg">
-            <img src={macbook.src} alt="MacBook" />
-            <div className="bg-green-900 pt-[200px]">
-              <div>
-              <p>FREELANCING 101 * Joseph Patrick</p>
-              <h2>07 Ways to get Consistent Clients from Social Media</h2>
-              <p>15 Min Read * 23 - 05 -2023</p>
-              </div>
-            </div>
-          </div>
-          </div>
-          <img src={macbook.src} alt="MacBook" />
-        </div> */}
+       
 
 
-<a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-  <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={macbook.src} alt="MacBook" />
+<a href="#" className="flex flex-col md:w-full md:h-72 items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row dark:border-gray-700 dark:bg-gray-800 ">
+  <img className=" h-full object-cover" src={macbook.src} alt="MacBook" />
   <div className="flex flex-col justify-between p-4 leading-normal">
     <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
     <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
