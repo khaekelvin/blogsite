de5cbb6a5f6f8826bb3a90a70672fd31857c7112
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-01 14:14:48 +0000

    card

--- app/Hero/page.tsx
+++ app/Hero/page.tsx
@@ -16,7 +16,7 @@ function Hero() {
             </p>
           </div>
         </div>
-        <div className="mt-8 m-3">
+        {/* <div className="mt-8 m-3">
           <div className="bg-blue-950 rounded-2xl p-2 ">
            
             <div className="bg-black rounded-lg">
@@ -30,8 +30,21 @@ function Hero() {
             </div>
           </div>
           </div>
-        </div>
+          <img src={macbook.src} alt="MacBook" />
+        </div> */}
+
+
+<a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
+  <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={macbook.src} alt="MacBook" />
+  <div className="flex flex-col justify-between p-4 leading-normal">
+    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+    <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+  </div>
+</a>
+
       </div>
+
+
     </>
   );
 }
