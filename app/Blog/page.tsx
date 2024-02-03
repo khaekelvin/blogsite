95a0c221c019e60643a7f7a6173943db7441e7ee
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-03 23:45:54 +0000

    blog text

--- app/Blog/page.tsx
+++ app/Blog/page.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 function blog() {
   return (
-    <div>page</div>
+    <>
+    <div>
+    <h1>Blog</h1>
+    </div>
+
+    <div>
+        <button>All Blogs</button>
+        <p>Trends</p>
+        <p>Research</p>
+        <p>Research</p>
+        <p>In-house Knowledge</p>
+    </div>
+    </>
   )
 }
 
