f9e7eb258a5a37cecdb287569e4cf358ec2d6f51
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-07 19:01:58 +0000

    new blog

--- app/Blog/page.tsx
+++ app/Blog/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import social from '../images/Frame 1249187079.png'
 
 function blog() {
   return (
@@ -21,6 +22,32 @@ function blog() {
     </div>
     </div>
     </div>
+
+    <div className="max-w-7xl mx-auto mb-10">
+
+        <div className="bg-white p-3 rounded-xl mt-8 text-center font-helvetica sm:mx-0 mx-2">
+          <a
+            href="#"
+            className="flex flex-col card md:w-full md:h-96 bg-white rounded-lg md:flex-row "
+          >
+            <img
+              className=" h-full object-cover rounded"
+              src={social.src}
+              alt="social"
+            />
+            <div className="flex flex-col leading-normal sm:text-center">
+              <div className="lg:pl-20 mt-auto mb-auto lg:text-left">
+                <p className="free text-lg mt-10 mb-5">FREELANCING 101 • William George</p>
+                <h1 className="text-4xl font-medium sm:break-words mb-10 lg:w-96">                  
+                  07 Ways to get Consistent Clients from Social
+                  Media
+                </h1>
+                <p className="min font-medium text-lg mb-10">15 Min Read • 23 - 05 -2023</p>
+              </div>
+            </div>
+          </a>
+        </div>
+      </div>
     </>
   )
 }
