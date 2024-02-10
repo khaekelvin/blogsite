6cfa53e77d4598e5dba2c60422fdc6d3d0f3f467
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-10 21:38:23 +0000

    hmm

--- app/Components/Card/Page.tsx
+++ app/Components/Card/Page.tsx
@@ -1,23 +1,19 @@
 import React from 'react'
 
+
 const cardData = [
-  {
-    title: "FREELANCING 101 • William George",
-    description: "07 Ways to get Consistent Clients from Social Media",
-    date: "2023-05-23",
-    imageSrc: "image1.jpg",
-  },
+  
   {
     title: "FREELANCING 101 • Christina Morillo",
     description: "Enhance UX: Designing Better UX/UI",
     date: "2023-05-24",
-    imageSrc: "image2.jpg",
+    imageSrc: "https://i.ibb.co/0sMDLNX/Group-1000004815.png",
   },
   {
     title: "FREELANCING 101 • Smith Jack",
     description: "Designing Impression: The Power of First Impressions",
     date: "2023-05-24",
-    imageSrc: "image3.jpg",
+    imageSrc: "https://i.ibb.co/d4Y23FL/3dicons.png",
   },
   {
     title: "FREELANCING 101 • Brian Smith",
@@ -37,47 +33,53 @@ const cardData = [
     date: "2023-05-24",
     imageSrc: "image6.jpg",
   },
+  {
+    title: "FREELANCING 101 • William George",
+    description: "Kickstart your UI/UX design career?",
+    date: "2023-05-23",
+    imageSrc: "image1.jpg",
+  },
 ];
 
 
-function Card() {
-  return (
-    <>
-      <div className="max-w-7xl mx-auto mb-10">
-        <div className=" mb-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
-          {cardData.map((data, index) => (
-            <div
-              key={index}
-              className="bg-white p-3 rounded-xl mt-8 text-center font-helvetica"
-            >
-              <a
-                href="#"
-                className="flex flex-col card md:w-full md:h-96 bg-white rounded-lg md:flex-row "
-              >
-                <img
-                  className=" h-full object-cover rounded"
-                  src={data.imageSrc}
-                  alt="social"
-                />
-                <div className="flex flex-col leading-normal sm:text-center">
-                  <div className="lg:pl-7 mt-auto mb-auto lg:text-left">
-                    <p className="free text-lg mt-10 mb-5">{data.title}</p>
-                    <h1 className="text-4xl font-medium sm:break-words mb-10 sm:mb-16 lg:w-96">
-                      {data.description}
-                    </h1>
-                    <p className="min font-medium text-lg mb-10">{`15 Min Read • ${data.date}`}</p>
-                  </div>
-                </div>
-              </a>
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
-  );
-}
+// function Card() {
+//   return (
+//     <>
+//       <div className="max-w-7xl mx-auto mb-10">
+//         <div className="mb-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+//           {cardData.map((data, index) => (
+//             <div
+//               key={index}
+//               className="bg-white p-3 rounded-xl mt-8 text-center font-helvetica"
+//             >
+//               <a
+//                 href="#"
+//                 className="flex flex-col card md:w-full md:h-96 bg-white rounded-lg md:flex-row "
+//               >
+//                 <div className="flex flex-col leading-normal lg:mx-5">
+//                   <img
+//                     className=" w-48 object-cover rounded"
+//                     src={data.imageSrc}
+//                     alt="social"
+//                   />
+//                   <div className="mt-auto mb-auto text-left">
+//                     <p className="free text-lg mt-10 mb-5">{data.title}</p>
+//                     <h1 className="text-3xl font-medium sm:break-words mb-10 sm:mb-16 lg:w-96">
+//                       {data.description}
+//                     </h1>
+//                     <p className="min font-medium text-lg mb-10">{`15 Min Read • ${data.date}`}</p>
+//                   </div>
+//                 </div>
+//               </a>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     </>
+//   );
+// }
 
-export default Card;
+// export default Card;
 
 
 // {
