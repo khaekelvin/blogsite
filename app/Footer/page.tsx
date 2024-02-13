4cab619784ae6204528924d3ed0d2539a5eeca92
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-13 15:22:46 +0000

    ei

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
 
 function page() {
   return (
     <>
-    <div>
-        a
-    </div>
+      <div className="foot w-full h-[600px] justify-items-center">
+        <div>
+          <div className="max-w-7xl bg-white rounded-xl h-[500px] mt-5 mx-auto flex ">
+            a
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default page
+export default page;
