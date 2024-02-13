d6113ea9e908de1ef7efe531891622283346c00a
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-13 00:49:06 +0000

    done for day

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -1,25 +1,31 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
-  return (
-    <>
-    <div className=' max-w-7xl mx-auto mt-10'>
-        <div className='foot rounded-xl h-auto md:h-[180px] mx-2 md:mx-0'>
-            <div className='w-[350px] mx-auto'>
-            <p className='text-white text-5xl pt-10 mb-5'>Transforming your <b>Ideas into reality</b></p>
-            <p className='text-white text-lg'>Let's build something extraordinary together to  captivate your audience.</p>
-            </div>
+    return (
+        <>
+            <div className="max-w-7xl mx-auto mt-10">
+                <div className="foot rounded-xl h-auto md:h-[300px] mx-2 md:mx-0">
+                    <div className="lg:flex lg:justify-between lg:max-w-5xl lg:mx-auto lg:justify-items-center">
+                        <div className="lg:w-[500px] lg:mx-0 mx-auto">
+                            <p className="text-white lg:text-6xl text-5xl lg:pt-0 pt-10 lg:mb-0 mb-5 leading-[60px]">
+                                Transforming your <b>Ideas into reality</b>
+                            </p>
+                            <p className="text-white text-xl">
+                                Let's build something extraordinary together to captivate your
+                                audience.
+                            </p>
+                        </div>
 
-            <div className='flex items-center justify-center'>
-                <p className='relative text-2xl w-60 h-60 rounded-full flex justify-center items-center text-center p-5 border-2 border-white text-white my-14'>
-                Let’s Work <br/> Together!
-                </p>
-
+                        <div className="flex items-center justify-center">
+                            <p className="relative text-2xl w-60 h-60 rounded-full flex justify-center items-center text-center p-5 border-2 border-white text-white lg:my-0 my-14">
+                                Let’s Work <br /> Together!
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
-        </div>
-    </div>
-    </>
-  )
+        </>
+    );
 }
 
-export default Footer
+export default Footer;
