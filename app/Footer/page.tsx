6866b621e60282d1781a329dd5440e502c006f16
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-14 01:28:41 +0000

    new

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -1,12 +1,20 @@
 import React from "react";
+import { FaInstagram, FaFacebook, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 function page() {
   return (
     <>
-      <div className="foot w-full h-[600px] justify-items-center">
+      <div className="foot w-full h-[600px] justify-items-center relative ">
         <div>
-          <div className="max-w-7xl bg-white rounded-xl h-[500px] mt-5 mx-auto flex ">
-            a
+          <div className="absolute inset-0 max-w-7xl bg-white rounded-xl h-[500px] lg:mx-auto my-auto mx-2">
+            <p className="font-bold hel text-xl flex justify-center mt-4">Say Hello!</p>
+            <p className="flex justify-center text-lg">Atlaspremier@gmail.com</p>
+            <div className="flex justify-center">
+              <FaInstagram/>
+              <FaFacebook/>
+              <FaTwitter/>
+              <FaLinkedin/>
+            </div>
           </div>
         </div>
       </div>
