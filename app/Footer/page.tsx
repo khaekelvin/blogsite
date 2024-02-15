086d878988bb93ec7eb2c1aaee20cf2e903905b1
Kelvin Duobu <130221570+khaekelvin@users.noreply.github.com>
2024-02-15 00:49:47 +0000

    text

--- app/Footer/page.tsx
+++ app/Footer/page.tsx
@@ -7,13 +7,37 @@ function page() {
       <div className="foot w-full h-[600px] justify-items-center relative ">
         <div>
           <div className="absolute inset-0 max-w-7xl bg-white rounded-xl h-[500px] lg:mx-auto my-auto mx-2">
-            <p className="font-bold hel text-xl flex justify-center mt-4">Say Hello!</p>
-            <p className="flex justify-center text-lg">Atlaspremier@gmail.com</p>
-            <div className="flex justify-between mx-10 mt-5">
-              <FaInstagram size={25} color="#294F74" />
-              <FaTwitter size={25} color="#294F74" />
-              <FaLinkedin size={25} color="#294F74" />
-              <FaFacebook size={25} color="#294F74" />
+            <div className="max-w-80 mx-auto">
+              <p className="font-bold hel text-xl flex justify-center mt-4">
+                Say Hello!
+              </p>
+              <p className="flex justify-center text-lg">
+                Atlaspremier@gmail.com
+              </p>
+              <div className="flex justify-between my-5">
+                <FaInstagram size={25} color="#294F74" />
+                <FaTwitter size={25} color="#294F74" />
+                <FaLinkedin size={25} color="#294F74" />
+                <FaFacebook size={25} color="#294F74" />
+              </div>
+            </div>
+
+            <div className="max-w-80 mx-auto text-xl">
+              <div className="flex justify-between text-left">
+                <p className="mb-3">Home</p>
+                <p className="mb-3">Services</p>
+              </div>
+
+              <div className="flex justify-between text-left">
+                <p className="mb-3">About us</p>
+                <p className="mb-3">Contact us</p>
+              </div>
+              <p>Work</p>
+            </div>
+
+            <div className="text-xl">
+              <p className="text-center my-5">NewYork, US</p>
+              <p className="text-center">AtlasPremier. All Rights Reserved</p>              
             </div>
           </div>
         </div>
